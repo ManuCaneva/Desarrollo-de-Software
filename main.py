40a2ee901fe317b2f0753747ef8507a73f298cdd
Tahiel <tahiel.utn@gmail.com>
2025-09-04 10:49:58 -0300

    modifico el main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Noticia:
     def __init__(self, id, titulo, categoria, cuerpo, fechaPublicacion):
         self.id = id
@@ -11,12 +10,3 @@ class Noticia:
     def conteoPalabras(self):
         #cuenta algo qsy
         return None
-=======
-"""
-jijiji ja
-"""
-
-"""
-jajaja ji
-"""
->>>>>>> 714647ea5d2eb386f7a5cceaf7f14078d920a8dd
