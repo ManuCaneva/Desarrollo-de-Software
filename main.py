cd81eabc936346ec5cf83fd99001d96e19c7476e
CarlaU13 <carlaurturi2013@gmail.com>
2025-09-04 11:06:40 -0300

    Trate de hacer contenido

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from enum import Enum
+from abc import ABC, abstractmethod
 
-Class Categoria(Enum):
+class Categoria(Enum):
     DEPORTES = 0
     SOCIALES = 1
     POLICIALES = 2
@@ -24,3 +25,32 @@ class Noticia:
 class Usuario:
     def __init__(self, nombre):
         self.nombre = nombre
+
+class Contenido(ABC):
+    @abstractmethod
+    def texto_extraible(self) -> str:
+        pass
+class Texto(Contenido):
+    def __init__(self, contenido: str):
+        self.contenido = contenido
+
+    def texto_extraible(self) -> str:
+        return self.contenido
+
+
+class Imagen(Contenido):
+    def __init__(self, descripcion: str):
+        self.descripcion = descripcion
+
+    def texto_extraible(self) -> str:
+        # simplificamos: devolvemos descripción como "texto extraíble"
+        return self.descripcion
+
+
+class Video(Contenido):
+    def __init__(self, url: str, descripcion: str = ""):
+        self.url = url
+        self.descripcion = descripcion
+
+    def texto_extraible(self) -> str:
+        return self.descripcion
