8ca24c4bf445155f4d28cce3d7e2c5c8fb4f463a
Azula-z <tomas-a21@outlook.com>
2025-09-04 10:59:11 -0300

    agregue categoria

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from enum import Enum
+
+Class Categoria(Enum):
+    DEPORTES = 0
+    SOCIALES = 1
+    POLICIALES = 2
+    ESPECTACULOS = 3
+    ECONOMIA = 4
+    INTERNACIONALES = 5
+    CHIMENTERO = 6
+
 class Noticia:
     def __init__(self, id, titulo, categoria, cuerpo, fechaPublicacion):
         self.id = id
