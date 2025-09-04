2eaa8e31dbe37e1ea50c6258f0f8597dea001216
Tahiel <tahiel.utn@gmail.com>
2025-09-04 10:39:39 -0300

    estoy probando de vuelta el tema del comentario xd

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-"""
-jijiji ja
-"""
+class Noticia:
+    def __init__(self, id, titulo, categoria, cuerpo, fechaPublicacion):
+        self.id = id
+        self.titulo = titulo
+        self.categoria = categoria
+        self.cuerpo = cuerpo
+        self.fechaPublicación = fechaPublicacion
+        pass
+
+    def conteoPalabras(self):
+        #cuenta algo qsy
+        return None
